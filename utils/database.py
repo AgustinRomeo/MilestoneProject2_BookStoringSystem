27f114fa1e1a2484ec4f10bcbfec6ca10c54345f
AgustinRomeo <hola@agustinromeo.com>
2023-05-17 12:29:24 -0300

    Update database.py
    Modified the database.py so it directly works with the csv file

--- utils/database.py
+++ utils/database.py
@@ -11,9 +11,9 @@ class Book:
 
     def to_dict(self):
         return {
-            "title": self.title,
-            "author": self.author,
-            "read": self.read
+            "Title": self.title,
+            "Author": self.author,
+            "Read": str(self.read)
         }
 
     def get_title(self):
@@ -23,94 +23,158 @@ class Book:
         return self.author
 
 
+def search_books(search_term, search_by="title"):
+    result = []
+    search_term = search_term.lower()
+
+    with open("books.csv", "r", newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if search_by.lower() == "title" and row["Title"].lower() == search_term:
+                book = Book(row["Title"], row["Author"], bool(row["Read"]))
+                result.append(book)
+            elif search_by.lower() == "author" and row["Author"].lower() == search_term:
+                book = Book(row["Title"], row["Author"], bool(row["Read"]))
+                result.append(book)
+            elif search_by.lower() == "read" and row["Read"].lower() == search_term:
+                book = Book(row["Title"], row["Author"], bool(row["Read"]))
+                result.append(book)
+
+    if not result:
+        print("No books found.")
+
+    return result
+
+
 class BookStore:
     def __init__(self):
         self.books = []
 
     def add_book(self, book):
-        for existing_book in self.books:
-            if existing_book.get_title().lower() == book.get_title().lower() and existing_book.get_author().lower() == book.get_author().lower():
-                print("You already own this book.")
-                return
-        self.books.append(book)
+        with open("books.csv", "a", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow([book.get_title(), book.get_author(), str(book.read)])
         print("Book added successfully.")
 
     def remove_book(self, book):
-        if book not in self.books:
+        with open("books.csv", "r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            books_data = list(reader)
+
+        # Find the index of the book in the CSV data
+        index = None
+        for i, row in enumerate(books_data):
+            if row[0].lower() == book.get_title().lower() and row[1].lower() == book.get_author().lower():
+                index = i
+                break
+
+        if index is None:
             print("The book is not in your collection.")
             return
-        self.books.remove(book)
+
+        # Remove the book from the CSV data
+        books_data.pop(index)
+
+        with open("books.csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Title", "Author", "Read"])
+            writer.writerows(books_data)
+
         print("Book removed successfully.")
 
     def list_books(self):
-        if not self.books:
+        with open("books.csv", "r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            books_data = list(reader)
+
+        if len(books_data) <= 1:
             print("Your collection is empty.")
             return
-        for book in self.books:
-            print(f"Title: {book.title}, Author: {book.author}, Read: {book.read}")
+
+        for row in books_data[1:]:
+            title, author, read = row
+            print(f"Title: {title}, Author: {author}, Read: {read}")
 
     def mark_book_as_read(self, title):
-        for book in self.books:
-            if book.title.lower() == title.lower():
-                book.read = True
-                print("Book marked as read.")
-                return
-        print("The book is not in your collection.")
+        with open("books.csv", "r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            books_data = list(reader)
 
-    def search_books(self, search_term, search_by="title"):
+        found = False
+        for row in books_data[1:]:
+            if row[0].lower() == title.lower():
+                row[2] = "True"
+                found = True
+                break
+
+        if not found:
+            print("The book is not in your collection.")
+            return
+
+        with open("books.csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerows(books_data)
+
+        print("Book marked as read.")
+
+    @staticmethod
+    def has_read_book(title):
+        with open("books.csv", "r", newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row["Title"].lower() == title.lower() and row["Read"].lower() == "true":
+                    return True
+        return False
+
+    @staticmethod
+    def has_book(title):
+        with open("books.csv", "r", newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row["Title"].lower() == title.lower():
+                    return True
+        return False
+
+    @staticmethod
+    def delete_book(title):
+        with open("books.csv", "r", newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            rows = list(reader)
+
+        found = False
+        with open("books.csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=["Title", "Author", "Read"])
+            writer.writeheader()
+            for row in rows:
+                if row["Title"].lower() == title.lower():
+                    found = True
+                else:
+                    writer.writerow(row)
+
+        if found:
+            print("Book deleted successfully.")
+        else:
+            print("The book is not in your collection.")
+
+    @staticmethod
+    def search_books(search_by, search_term):
         result = []
         search_term = search_term.lower()
 
-        for book in self.books:
-            if search_by.lower() == "title" and book.get_title().lower() == search_term:
-                result.append(book)
-            elif search_by.lower() == "author" and book.get_author().lower() == search_term:
-                result.append(book)
-            elif search_by.lower() == "read" and book.read == (search_term == "read"):
-                result.append(book)
+        with open("books.csv", "r", newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if search_by.lower() == "title" and row["Title"].lower() == search_term:
+                    book = Book(row["Title"], row["Author"], bool(row["Read"]))
+                    result.append(book)
+                elif search_by.lower() == "author" and row["Author"].lower() == search_term:
+                    book = Book(row["Title"], row["Author"], bool(row["Read"]))
+                    result.append(book)
+                elif search_by.lower() == "read" and row["Read"].lower() == search_term:
+                    book = Book(row["Title"], row["Author"], bool(row["Read"]))
+                    result.append(book)
 
         if not result:
             print("No books found.")
-            return
-        for book in result:
-            print(f"Title: {book.title}, Author: {book.author}, Read: {book.read}")
 
-    def has_read_book(self, title):
-        for book in self.books:
-            if book.get_title().lower() == title.lower() and book.read:
-                return True
-        return False
-
-    def has_book(self, title):
-        for book in self.books:
-            if book.get_title().lower() == title.lower():
-                return True
-        return False
-
-    def delete_book(self, title):
-        for book in self.books:
-            if book.title.lower() == title.lower():
-                self.books.remove(book)
-                print("Book deleted successfully.")
-                return
-        print("The book is not in your collection.")
-
-    def save_books_to_csv(self, filename):
-        with open(filename, "w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(["Title", "Author", "Read"])
-            for book in self.books:
-                writer.writerow([book.get_title(), book.get_author(), str(book.read)])
-        print("Books saved to CSV successfully.")
-
-    def load_books_from_csv(self, filename):
-        self.books = []
-        with open(filename, "r") as file:
-            reader = csv.reader(file)
-            header = next(reader)  # Skip the header row
-            for row in reader:
-                title, author, read = row
-                book = Book(title, author)
-                book.read = bool(read)  # Convert the read value to boolean
-                self.add_book(book)
-        print("Books loaded from CSV successfully.")
+        return result
